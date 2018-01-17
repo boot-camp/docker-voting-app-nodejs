33552a96cfc07f88ea8ded97b302c6f28fd253b3
agcorp.io <widjaja_a@msn.com>
2018-01-17 23:42:48 +0800

    u s

--- stack.sh
+++ stack.sh
@@ -13,11 +13,11 @@
 
 # worker
 
-docker build -t registry.gitlab.com/bootcamp-juniors/vote/worker -f ./src/worker/Dockerfile ./src/worker/.
+docker build -t registry.gitlab.com/bootcamp-juniors/vote/worker:build -f ./src/worker/Dockerfile ./src/worker/.
 docker push registry.gitlab.com/bootcamp-juniors/vote/worker:build
 # app
 
-docker build -t registry.gitlab.com/bootcamp-juniors/vote/vote -f ./src/vote/Dockerfile ./src/vote/.
+docker build -t registry.gitlab.com/bootcamp-juniors/vote/vote:build -f ./src/vote/Dockerfile ./src/vote/.
 docker push registry.gitlab.com/bootcamp-juniors/vote/vote:build
 # api
 
@@ -26,5 +26,5 @@ docker push registry.gitlab.com/bootcamp-juniors/vote/vote:build
 
 # Monitoring
 
-docker build -t registry.gitlab.com/bootcamp-juniors/vote/auditor -f ./src/auditor/Dockerfile ./src/vote/.
+docker build -t registry.gitlab.com/bootcamp-juniors/vote/auditor:build -f ./src/auditor/Dockerfile ./src/auditor/.
 docker push registry.gitlab.com/bootcamp-juniors/vote/auditor:build
